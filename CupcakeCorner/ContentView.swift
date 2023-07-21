a986411d9ebaad2536002f12b7f3e5bed4a75a8b
Rob Ranf <rob@emiyaconsulting.com>
2023-07-20 18:57:57 -0700

    Can't get JSONDecoder to work

--- CupcakeCorner/ContentView.swift
+++ CupcakeCorner/ContentView.swift
@@ -7,15 +7,56 @@
 
 import SwiftUI
 
+struct Response: Codable {
+    var results: [Result]
+}
+
+struct Result: Codable {
+    var trackID: Int
+    var trackName: String
+    var collectionName: String
+}
+
 struct ContentView: View {
+    @State private var results = [Result]()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        List(results, id: \.trackID) { item in
+            VStack(alignment: .leading) {
+                Text(item.trackName)
+                    .font(.headline)
+                Text(item.collectionName)
+            }
         }
-        .padding()
+        .task {
+            await loadData()
+            print("task ran")
+        }
+    }
+    
+    func loadData() async {
+        guard let url = URL(
+            string: "https://itunes.apple.com/search?term=taylor+swift&entity=song") else {
+            print("Invalid URL")
+            return
+        }
+        
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            print("url session running")
+            print(data)
+            
+            let decoder = JSONDecoder()
+            
+            if let decodedResponse = try? decoder.decode(Response.self, from: data) {
+                print("Running before results...")
+                results = decodedResponse.results
+                print(results)
+                print("Running")
+            }
+        } catch {
+            print("Invalid data")
+        }
     }
 }
 
